ccfafd40d0acfff6b48179baf8c8c625554dab4b
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2016-02-07 23:20:07 -0500

    Started Libgdx project

---
+++ desktop/src/ca/almacin/climbgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.almacin.climbgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.almacin.climbgame.ClimbGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClimbGame(), config);
+	}
+}
